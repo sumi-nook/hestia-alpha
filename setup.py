b282a4f19b999e144d2d72ce2abb7451c3a996db
sumi.nook <sumi.nook@live.jp>
2016-05-06 19:31:00 +0900

    setup.py追加

---
+++ setup.py
@@ -0,0 +1,24 @@
+import py2exe
+from distutils.core import setup
+
+py2exe_options = {
+    "compressed": 1,
+    "optimize": 2,
+    "bundle_files": 1,
+    "includes" : ["sip", "lxml._elementpath"],
+    "excludes": [],
+    "dll_excludes": ["w9xpopen.exe",]
+}
+
+setup(
+    options={"py2exe" : py2exe_options},
+    windows=[{"script" : "main.py",
+              "dest_base": "Hestia",
+              # version info.
+              "version": "0.0.0.1",
+              "name": "Hestia",
+              "company_name:": "nook",
+              "copyright": "Copyright (c) 2016 S2_SUMI.",
+              "description": "Common Intermediate Script System."}],
+    zipfile=None,
+)
