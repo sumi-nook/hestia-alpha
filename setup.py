d04a341e879f9f0c7ece24ad7577a8aa69b78946
sumi.nook <sumi.nook@live.jp>
2016-05-10 16:53:01 +0900

    setup.py修正

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ py2exe_options = {
     "compressed": 1,
     "optimize": 2,
     "bundle_files": 1,
-    "includes" : ["sip", "lxml._elementpath"],
+    "includes" : ["sip", "lxml._elementpath",
+                  "markdown.extensions.nl2br"],
     "excludes": [],
     "dll_excludes": ["w9xpopen.exe",]
 }
